a6e0917f3409b77879f9c0b723c4d3f2f4359213
Glen Souza <glenster75@gmail.com>
2024-03-13 13:00:53 -0700

    votes

--- NJDOT.Models/Car.cs
+++ NJDOT.Models/Car.cs
@@ -2,5 +2,9 @@
 
 public class Car
 {
-    public string Name { get; set; } = string.Empty;
+    public string CarName { get; set; } = string.Empty;
+    public string CarImage { get; set; } = string.Empty;
+    public double Score { get; set; } = 1200;
+    public int Wins { get; set; } = 0;
+    public int Losses { get; set; } = 0;
 }
---
+++ NJDOT.Models/Vote.cs
@@ -0,0 +1,7 @@
+﻿namespace NJDOT.Models;
+
+public class Vote
+{
+    public string Car1 { get; set; } = string.Empty;
+    public string Car2 { get; set; } = string.Empty;
+}
